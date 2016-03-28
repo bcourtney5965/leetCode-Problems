7f500ef6ca6d87b85838391d40575e261e5be2bf
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-03-28 13:27:17 -0700

    psuedo coded maxDepthBinTree

---
+++ maxDepthBinTree.js
@@ -0,0 +1,41 @@
+// i: input - binary tree
+// o: ouput - integer
+// c: constraints
+
+// * notes to Zack or myself
+
+// PSEUDO CODE
+// var maxDepth = function(root) {
+	// initialize depth = 0
+	// initialize closure
+		// increment depth
+		// if no chilren
+			// return depth
+		// if children
+			// invoke closure on curr node
+	// invoke closure on root
+	// return depth
+// };
+
+
+// CODE
+
+/**
+Write line by line pseudocode for each 
+solution prior to starting with actual 
+code
+
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    
+};
+
