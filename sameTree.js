e1a5ec224a4a69a3b43133407f1bf2ab5acc1dd2
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-04-11 00:24:42 -0700

    Superior approach - attempt

--- sameTree.js
+++ sameTree.js
@@ -30,92 +30,19 @@
 
 // PSEUDO CODE
 // initialize isSameTree function with two params- two objects (p, q)
-  // initialize output variable to true
-  // initialize treeTraverser function(pNode, qNode)
-    // if p and q's values aren't equal
-      // set output to false
-          // left side
-    // if p.left equals null and q.left doesn't equal null
-      // set output to false
-    // if p.left doesn't equal null and q.left does equal null
-      // set output to false
-  
-          // right side
-    // if p.right equals null and q.right doesn't equal null
-      // set output to false
-    // if p.right doesn't equal null and q.right does equal null
-      // set output to false
-  
-          // ensuring recursion only happens on objects
-    // if p.left and q.left both don't equal null
-      // invoke treeTraverser(p.left, q.left);
-    // if p.right and q.right both don't equal null
-      // invoke treeTraverser(p.right, q.right);
-  // close treeTraverser function
-  }
-  // invoke treeTraverser on (p, q)
-  // return output
+  // if p.val doens't equal q.val
+    // return false
+  // if p.val and q.val both equal null
+    // return true
+  // if p.left and q.left don't equal null
+    // return isSameTree(p.left, q.left)
+  // if p.right and q.right don't equal null
+    // return isSameTree(p.right, q.right)  
 // close function 
-}
+
   
 
 
 
 
 // CODE
-// PSEUDO CODE
-// initialize isSameTree function with two params- two objects (p, q)
-var isSameTree = function(p, q) {
-  // initialize output variable to true
-  var output = true; 
-  // initialize treeTraverser function(pNode, qNode)
-  var treeTraverser = function(pNode, qNode) {
-    // if p and q's values aren't equal
-    if (p.val !== q.val) {
-      // set output to false
-      output = false;
-    }
-    
-          // left side
-    // if p.left equals null and q.left doesn't equal null
-    if (p.left === null && q.left !== null) {
-      // set output to false
-      output = false;
-    }
-    // if p.left doesn't equal null and q.left does equal null
-    if (p.left !== null && q.left === null) {
-      // set output to false
-      output = false;
-    }
-
-          // right side
-    if (p.right === null && q.right !== null) {
-      // set output to false
-      output = false;
-    }
-    // if p.right doesn't equal null and q.right does equal null
-    if (p.right !== null && q.right === null) {
-      // set output to false
-      output = false;
-    }
-  
-          // ensuring recursion only happens on objects
-    // if p.left and q.left both don't equal null
-    if (p.left !== null && q.left !== null) {
-      // invoke treeTraverser(p.left, q.left);
-      treeTraverser(p.left, q.left)
-    }
-    // if p.right and q.right both don't equal null
-    if (p.right && q.right) {
-      // invoke treeTraverser(p.right, q.right);
-      treeTraverser(p.right, q.right);
-    }
-  // close treeTraverser function
-  }
-  // invoke treeTraverser on (p, q)
-  treeTraverser(p, q)
-
-  // return output
-  return output;
-// close function 
-}
