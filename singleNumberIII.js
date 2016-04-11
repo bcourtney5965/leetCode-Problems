7f08c9da555c44252c00e7d38f2171fe2fc6f7e7
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-04-10 23:00:53 -0700

    SingleNumberIII complete successful

--- singleNumberIII.js
+++ singleNumberIII.js
@@ -1,6 +1,6 @@
 // i: input - array
 // o: ouput - array
-// c: constraints - constant time complexity
+// c: constraints - linear time complexity
 
 // * notes to Zack or myself
 
@@ -23,13 +23,50 @@
 // PSEUDO CODE
 
 // initilize function param: array
-
-
-    
-
-
-
+  // inititialize numbers object & output array
+  // loop over array
+    // if an element exists in numbers object increment the value
+    // else 
+      // add each element as a key and set value to 1;
+  // close loop
+  // loop through object
+    // if value equals 2
+      // push key to array
+  // close loop
+  // return output array
+// close function
 
 
 
 // CODE
+
+// initilize function param: array
+var singleNumberFinder = function(arr) {
+  // inititialize numbers object & output array
+  var numbers = {}, output = [];
+  // loop over array
+  for (var i = 0; i < arr.length; i++) {
+    // if an element exists in numbers object
+    if (numbers[arr[i]]) {
+      // increment the value
+      numbers[arr[i]]++;
+    // else  
+    } else {
+      // add each element as a key and set value to 1;
+      numbers[arr[i]] = 1;
+    }
+  // close loop
+  }
+  // loop through object
+  for (var key in numbers) {
+    // if value equals 2
+    if (numbers[key] === 1) {
+      // push key to array
+      output.push(key);
+    }
+  // close loop
+  }
+  // return output array
+  return output;
+// close function
+};
