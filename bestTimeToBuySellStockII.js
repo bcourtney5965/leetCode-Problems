859ddfcc2e39c2fe4466720e9ef85ff0114fd103
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-04-17 23:50:27 -0700

    started bestTimetoBuySellStock

---
+++ bestTimeToBuySellStockII.js
@@ -0,0 +1,27 @@
+// i: input - array
+// o: ouput - number
+// c: constraints - can't have multiple trades at the same time
+
+// * notes to Zack or myself
+
+// Instructions
+// Say you have an array for which the ith element is the price of a given 
+// stock on day i.
+
+// Design an algorithm to find the maximum profit. You may complete as many 
+// transactions as you like (ie, buy one and sell one share of the stock 
+// multiple times). However, you may not engage in multiple transactions 
+// at the same time (ie, you must sell the stock before you buy again).
+
+// PSEUDO CODE
+// some approaches
+  // biggestnumber - smallestNumber
+  // if i + 1 > 1 take the profti, repeat for length of array
+
+
+
+// thoughts: 
+  // seems like it should be recursive
+
+
+// CODE
